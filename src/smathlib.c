8a711a6f2b8b4f954900bdf562c3847a40812c38
unknown <andypythonappdeveloper@gmail.com>
2020-11-21 17:06:46 +1100

    Add Trigonometry Functions

--- src/smathlib.c
+++ src/smathlib.c
@@ -31,7 +31,52 @@ static Constant sglRemap(VM* vm, int arity, Constant* args) {
   return NUM_CONST((value - fromMin ) * (toMax - toMin) / (fromMax - fromMin) + toMin);
 }
 
+static Constant sglSin(VM* vm, int arity, Constant* args) { 
+  double angle = AS_NUMBER(args[0]);
+
+  return NUM_CONST(sin(angle));
+}
+
+static Constant sglCos(VM* vm, int arity, Constant* args) { 
+  double angle = AS_NUMBER(args[0]);
+
+  return NUM_CONST(cos(angle));
+}
+
+static Constant sglTan(VM* vm, int arity, Constant* args) { 
+  double angle = AS_NUMBER(args[0]);
+
+  return NUM_CONST(tan(angle));
+}
+
+static Constant sglAsin(VM* vm, int arity, Constant* args) { 
+  double value = AS_NUMBER(args[0]);
+
+  return NUM_CONST(asin(value));
+}
+
+static Constant sglAcos(VM* vm, int arity, Constant* args) { 
+  double value = AS_NUMBER(args[0]);
+
+  return NUM_CONST(acos(value));
+}
+
+static Constant sglAtan(VM* vm, int arity, Constant* args) { 
+  double value = AS_NUMBER(args[0]);
+
+  return NUM_CONST(atan(value));
+}
+
+static Constant sglAtan2(VM* vm, int arity, Constant* args) { 
+  double y = AS_NUMBER(args[0]);
+  double x = AS_NUMBER(args[1]);
+
+  return NUM_CONST(atan2(y, x));
+}
+
 Constant initMathLib(VM* vm, int arity, Constant* args) {
+  // ============= VARIABLES =============
+
   // Base of natural logarithms, e.
   defineGlobal(vm, "e", NUM_CONST(2.718281828459045));
 
@@ -55,6 +100,8 @@ Constant initMathLib(VM* vm, int arity, Constant* args) {
   // Square root of 2
   defineGlobal(vm, "sqrt2", NUM_CONST(1.4142135623730951));
 
+  // ============= FUNCTIONS =============
+
   // Determines the smallest value between 2 numbers, and then returns that value.
   defineGlobalFunc(vm, "min", sglMin);
 
@@ -63,4 +110,26 @@ Constant initMathLib(VM* vm, int arity, Constant* args) {
 
   // Re-maps a number from one range to another.
   defineGlobalFunc(vm, "remap", sglRemap);
+
+  // Calculates the sine of an angle. Values are returned in the range -1 to 1.
+  defineGlobalFunc(vm, "sin", sglSin);
+
+  // Calculates the cosine of an angle. Values are returned in the range -1 to 1.
+  defineGlobalFunc(vm, "cos", sglCos);
+
+  // Calculates the tangent of an angle. Values are returned in the range of all real numbers.
+  defineGlobalFunc(vm, "tan", sglTan);
+
+  // The inverse of sin(), returns the arc sine of a value.
+  defineGlobalFunc(vm, "asin", sglAsin);
+
+  // The inverse of cos(), returns the arc cosine of a value.
+  defineGlobalFunc(vm, "acos", sglAcos);
+
+  // The inverse of tan(), returns the arc tangent of a value.
+  defineGlobalFunc(vm, "atan", sglAtan);
+
+  // Calculates the angle (in radians) from a specified point to the coordinate origin as measured from the positive x-axis.
+  // Note: The y-coordinate of the point is the first parameter, and the x-coordinate is the second parameter, due the the structure of calculating the tangent.
+  defineGlobalFunc(vm, "atan2", sglAtan2);
 }
