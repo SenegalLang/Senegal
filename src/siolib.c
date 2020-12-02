173177e6e13db597b291ea7faffb51cace2a8f1c
unknown <andypythonappdeveloper@gmail.com>
2020-12-02 17:23:15 +1100

    Remove the sglSystem function for now

--- src/siolib.c
+++ src/siolib.c
@@ -12,16 +12,7 @@ static Constant sglExit(VM* vm, int arity, Constant* args) {
   return NULL_CONST;
 }
 
-static Constant sglSystem(VM* vm, int arity, Constant* args) {
-  char* command = AS_CSTRING(args[0]);
-
-  return NUM_CONST(system(command));
-}
-
 Constant initIoLib(VM* vm, int arity, Constant* args) {
   // Terminate the process immediately and the exit code is returned to the parent process.
   defineGlobalFunc(vm, "exit", sglExit);
-
-  // Excutes the command passed as a string in the command processor or the terminal of the operating system.
-  defineGlobalFunc(vm, "system", sglSystem);
 }
