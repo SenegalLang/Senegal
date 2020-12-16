019d23c66186893053277039ae5a25f898802b55
Calamity210 <Calamity210@users.noreply.github.com>
2020-12-16 18:44:10 -0500

    Improved README and docs/index.js

--- README.md
+++ README.md
@@ -1,8 +1,33 @@
 <p align="center"><img src="misc/logo.png" height="150px"></p>
 <h1 align="center">Senegal</h1>
 
-# Contribution
-- Join the discord - https://discord.gg/9dq6YB2
+Senegal is a powerful, small-but-fast, concurrent, class-based, and dynamically-typed programming language with a modern syntax.
 
-# Docs
-https://lang-senegal.web.app/
+## Sample Senegal Program
+```typescript
+class Senegal {
+    var name;
+
+    Senegal(name) {
+        this.name = name;
+    }
+    
+    function talk(msg) {
+        println(this.name + " says: " + msg);
+    }
+}
+
+var mel = Senegal("Mel");
+mel.talk("Hello world!"); // Mel says: Hello World
+```
+
+## Get Started
+Have I piqued your interest? Great! Head over to our [docs](https://lang-senegal.web.app) to get started.
+
+## Contributed
+Feel free to join in and help out with Senegal. You can start by
+- Joining the discord - https://discord.gg/9dq6YB2
+- and reading the style guide - https://lang-senegal.web.app/docs/Contribute/style
+
+## Documentation
+The documentation is still a work in progress, but you can find it at: https://lang-senegal.web.app/
--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -11,19 +11,33 @@ function Home() {
   const context = useDocusaurusContext();
   const {siteConfig = {}} = context;
   return (
-    <Layout
-      title={siteConfig.title}
-      description="Senegal Programming Language">
-      <div className={styles.hero}>
-        <header>
-          <img src={useBaseUrl('img/logo.svg')} />
-          <h1>{siteConfig.title}</h1>
-          <div className={styles.buttons}>
-            <Link to={useBaseUrl('docs/')}>Get Started</Link>
-          </div>
-        </header>
-      </div>
-    </Layout>
+      <Layout
+          title={siteConfig.title}
+          description="Smol Programming Language">
+        <div className={styles.hero}>
+          <header>
+            <img src={useBaseUrl('img/logo.svg')} />
+            <h1>{siteConfig.title}</h1>
+            <p>{siteConfig.tagline}</p>
+            <div className={styles.buttons}>
+              <Link to={useBaseUrl('docs/')}>Get Started</Link>
+            </div>
+          </header>
+          <main>
+            <section className={styles.section}>
+              <h2>Contribution</h2>
+              <ul>
+                <li>Clone the repo from https://github.com/SenegalLang/Senegal</li>
+                <li>Navigate to the new directory and build it by running:
+                  <div class="language-shell highlighter-rouge"><div class="highlight"><pre class="highlight language-shell"><code class=" language-shell">$ cmake --build .
+                  </code></pre></div>    </div>
+                </li>
+                <li>When ready, open and pull request and request a review from a team member.</li>
+              </ul>
+            </section>
+          </main>
+        </div>
+      </Layout>
   );
 }
 
