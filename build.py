4b8cfa8906b78cc3535dca54cb98b22b3a69410b
unknown <andypythonappdeveloper@gmail.com>
2020-12-22 14:49:45 +1100

    Add the build.py file

---
+++ build.py
@@ -0,0 +1,44 @@
+import argparse
+import os
+import platform
+import shutil
+
+def build(build_type: str, run: bool, test: bool):
+    if not os.path.exists("build"):
+        os.mkdir("build")
+
+        if platform.system().lower() == "windows":
+            os.system("cd build && cmake .. -G \"MinGW Makefiles\"")
+
+        else:
+            os.system("cd build && cmake ..")
+    
+    if build_type.lower() == "release":
+        os.system("cd build && cmake --build . --config Release")
+
+        shutil.copy(os.path.join("build", "senegal.exe"), os.path.join("test", "senegal.exe"))
+    else:
+        os.system("cd build && cmake --build . --config Debug")
+
+        shutil.copy(os.path.join("build", "senegal.exe"), os.path.join("test", "senegal.exe"))
+
+
+    if test:
+        os.system("cd test && pub get && pub run test")
+
+    if run:
+        os.system(os.path.join("build", "senegal.exe"))
+
+def main():
+    parser = argparse.ArgumentParser(description="A small script that helps to build Senegal lang easily.")
+
+    parser.add_argument("[RELEASE | DEBUG]", type=str, help="The build type,")
+    parser.add_argument("--run", "-r", help="Run Senegal right after the build process.", action="store_true")
+    parser.add_argument("--test", "-t", help="Run Senegal tests right after the build process.", action="store_true")
+
+    args = vars(parser.parse_args())
+    
+    build(args["[RELEASE | DEBUG]"], args["run"], args["test"])
+
+if __name__ == "__main__":
+    main()
